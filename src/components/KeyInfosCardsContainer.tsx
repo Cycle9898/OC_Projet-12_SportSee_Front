e1e88441e97a3564291c46b5244098f94d4e2a34
Cycle9898 <cycle9898@gmail.com>
2024-02-12 18:08:13 +0100

    refactor: added id property in keyInfosDataArray to replace index by id in the key attribute of KeyInfosCard mapping, in KeyInfosCardsContainer component

--- src/components/KeyInfosCardsContainer.tsx
+++ src/components/KeyInfosCardsContainer.tsx
@@ -5,55 +5,68 @@ import fatIcon from "../assets/icon/fat-icon.png";
 import KeyInfosCard from "./KeyInfosCard";
 
 type Props = {
-    keyInfosData: {
-        calorieCount: number,
-        proteinCount: number,
-        carbohydrateCount: number,
-        lipidCount: number
-    }
-}
+	keyInfosData: {
+		calorieCount: number;
+		proteinCount: number;
+		carbohydrateCount: number;
+		lipidCount: number;
+	};
+};
 
 export type KeyInfosDataObj = {
-    title: string;
-    iconURL: string;
-    value: string;
-}
+	id?: string;
+	title: string;
+	iconURL: string;
+	value: string;
+};
 
 /* 
 React component that represent the container for the key infos cards
 */
 function KeyInfosCardsContainer({ keyInfosData }: Props) {
-    //Store all needed data in an array to map on it later
-    const keyInfosDataArray: KeyInfosDataObj[] = [
-        {
-            title: "Calories",
-            iconURL: caloriesIcon,
-            value: `${(keyInfosData.calorieCount / 1000).toLocaleString('fr-FR',{ minimumFractionDigits: 3 })} kCal`
-        },
-        {
-            title: "Protéines",
-            iconURL: proteinIcon,
-            value: `${keyInfosData.proteinCount} g`
-        },
-        {
-            title: "Glucides",
-            iconURL: carbsIcon,
-            value: `${keyInfosData.carbohydrateCount} g`
-        },
-        {
-            title: "Lipides",
-            iconURL: fatIcon,
-            value: `${keyInfosData.lipidCount} g`
-        }
-    ];
+	//Store all needed data in an array to map on it later
+	const keyInfosDataArray: KeyInfosDataObj[] = [
+		{
+			id: "Cal01",
+			title: "Calories",
+			iconURL: caloriesIcon,
+			value: `${(keyInfosData.calorieCount / 1000).toLocaleString(
+				"fr-FR",
+				{ minimumFractionDigits: 3 }
+			)} kCal`
+		},
+		{
+			id: "Prot02",
+			title: "Protéines",
+			iconURL: proteinIcon,
+			value: `${keyInfosData.proteinCount} g`
+		},
+		{
+			id: "Glu03",
+			title: "Glucides",
+			iconURL: carbsIcon,
+			value: `${keyInfosData.carbohydrateCount} g`
+		},
+		{
+			id: "Lip04",
+			title: "Lipides",
+			iconURL: fatIcon,
+			value: `${keyInfosData.lipidCount} g`
+		}
+	];
 
-    return (<div className="key-info-container">
-        {keyInfosDataArray.map((keyInfosData,index) => <KeyInfosCard key={index}
-            title={keyInfosData.title}
-            iconURL={keyInfosData.iconURL}
-            value={keyInfosData.value} />
-        )}
-    </div>);
+	return (
+		<div className="key-info-container">
+			{keyInfosDataArray.map(keyInfosData => (
+				<KeyInfosCard
+					key={keyInfosData.id}
+					title={keyInfosData.title}
+					iconURL={keyInfosData.iconURL}
+					value={keyInfosData.value}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default KeyInfosCardsContainer;
