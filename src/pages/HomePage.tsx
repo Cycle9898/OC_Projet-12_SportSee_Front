4b17be18b47ce160c8f95f62fcaf94ff54bef3bd
Cycle9898 <cycle9898@gmail.com>
2024-02-15 17:39:40 +0100

    feat: added logics on homepage buttons to store user ID in Context and redirect on profile page + handled mocked data case

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,22 @@
+import { useContext } from "react";
+import { UserIDContext } from "../utils/Context";
 import mainLogo from "../assets/logo/logo.png";
+import { useNavigate } from "react-router-dom";
 
 /* 
 React component that represent the Profile Page
 */
 function HomePage() {
+	//Hooks related
+	const setUserID = useContext(UserIDContext).setUserID;
+	const navigate = useNavigate();
+
+	const handleUserClick = (id?: number) => {
+		//set id if it's provided (API data) and/or redirect to profile page
+		id && setUserID(id);
+		navigate("/profile");
+	};
+
 	return (
 		<>
 			<main className="Home-main">
@@ -17,8 +30,21 @@ function HomePage() {
 				</h1>
 
 				<div className="Home-main__buttons">
-					<button>Karl</button>
-					<button>Cecilia</button>
+					{process.env.REACT_APP_API_URL ? (
+						<>
+							<button onClick={() => handleUserClick(12)}>
+								Karl
+							</button>
+
+							<button onClick={() => handleUserClick(18)}>
+								Cecilia
+							</button>
+						</>
+					) : (
+						<button onClick={() => handleUserClick()}>
+							Utilisateur factice
+						</button>
+					)}
 				</div>
 			</main>
 		</>
